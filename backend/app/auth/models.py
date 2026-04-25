1db2e6a0937d6378da5b0d12d25de2c6271f7385
Yermakhan Aimukhamedov <yermakhan.aimukhamedov@gmail.com>
2026-04-26 02:02:52 +0500

    migrate to v2

--- backend/app/auth/models.py
+++ backend/app/auth/models.py
@@ -1,7 +1,8 @@
 import uuid
 
-from sqlalchemy import Boolean, Column, LargeBinary, String
+from sqlalchemy import Boolean, LargeBinary, String
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column
 
 from backend.app.database import Base
 from backend.app.mixins import TimeStampMixin
@@ -10,7 +11,9 @@ from backend.app.mixins import TimeStampMixin
 class User(Base, TimeStampMixin):
     __repr_attrs__ = ["id", "email"]
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    email = Column(String(255), unique=True, nullable=False)
-    password = Column(LargeBinary, nullable=False)
-    is_active = Column(Boolean, nullable=False, default=True)
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+    )
+    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    password: Mapped[bytes] = mapped_column(LargeBinary, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
