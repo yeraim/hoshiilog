b1fe3fed45b22b64ee4b687ba117cf3a6541fe18
Yermakhan Aimukhamedov <yermakhan.aimukhamedov@gmail.com>
2026-04-26 22:20:15 +0500

    add friends model

--- backend/app/auth/models.py
+++ backend/app/auth/models.py
@@ -1,8 +1,8 @@
 import uuid
 
-from sqlalchemy import Boolean, LargeBinary, String
+from sqlalchemy import Boolean, ForeignKey, LargeBinary, String, UniqueConstraint
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from backend.app.database import Base
 from backend.app.mixins import TimeStampMixin
@@ -17,3 +17,27 @@ class User(Base, TimeStampMixin):
     email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
     password: Mapped[bytes] = mapped_column(LargeBinary, nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
+
+    friends: Mapped[list["Friends"]] = relationship(
+        "Friends", foreign_keys="Friends.person_id", back_populates="person"
+    )
+
+
+class Friends(Base, TimeStampMixin):
+    __repr_attrs__ = ["id"]
+    __table_args__ = (UniqueConstraint("person_id", "friend_id"),)
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+    )
+    person_id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), ForeignKey("user.id"), nullable=False
+    )
+    friend_id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), ForeignKey("user.id"), nullable=False
+    )
+
+    person: Mapped["User"] = relationship(
+        "User", foreign_keys=[person_id], back_populates="friends"
+    )
+    friend: Mapped["User"] = relationship("User", foreign_keys=[friend_id])
