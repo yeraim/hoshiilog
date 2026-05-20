ad355f7669224461218bb3edb3579ed57e37f42b
Yermakhan Aimukhamedov <yermakhan.aimukhamedov@c-network.kz>
2026-05-20 11:56:19 +0500

    add event model

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,2 +1,3 @@
 from backend.app.auth.models import User  # type: ignore # noqa: F401
+from backend.app.events.models import Event  # noqa: F401
 from backend.app.wishes.models import Wish  # type: ignore # noqa: F401
---
+++ backend/app/events/models.py
@@ -0,0 +1,40 @@
+import enum
+import uuid
+from decimal import Decimal
+
+from backend.app.auth.models import User
+from backend.app.database import Base
+from backend.app.mixins import TimeStampMixin
+from sqlalchemy import Enum, ForeignKey, Numeric, String, UniqueConstraint
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+
+class EventStatus(enum.Enum):
+    PLANNING = 0
+    ACTIVE = 1
+    COMPLETED = 2
+    CANCELLED = 3
+
+
+class Event(Base, TimeStampMixin):
+    __repr_attrs__ = ["id"]
+    __table_args__ = (UniqueConstraint("title", "user_id"),)
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+    )
+    user_id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), ForeignKey("user.id")
+    )
+    title: Mapped[str] = mapped_column(String(255))
+    description: Mapped[str | None] = mapped_column(String(255))
+    image_url: Mapped[str | None] = mapped_column(String(255))
+    status: Mapped[EventStatus] = mapped_column(
+        Enum(EventStatus), default=EventStatus.PLANNING
+    )
+    price_limit: Mapped[Decimal] = mapped_column(Numeric(precision=10, scale=2))
+
+    owner: Mapped["User"] = relationship(
+        "User", foreign_keys=[user_id], back_populates="events"
+    )
